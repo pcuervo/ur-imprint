49543a3f7e6d3dc3d041c98849a730153a3de331
Raúl De Zamacona <raul@zamacona.me>
2015-07-30 19:42:10 -0500

    full video controls

--- themes/ur-imprint/index.php
+++ themes/ur-imprint/index.php
@@ -3,16 +3,20 @@
 	<!-- =================================================
 	==== HERO
 	================================================== -->
-	<section class="[ hero ][ padding ][ margin-bottom ]" style="background-image: url('images/background.jpg')">
-		<div class="[ wrapper ]">
-			<div class="[ text-center color-light ]">
-				<div class="[ bg-logo__wrapper ]">
-					<a class="[ bg-logo ]" href="#"></a>
-				</div>
-				<h2 class="[ text-shadow ]">Bring your imagination to life</h2>
-				<a href="#" class="[ button button--secondary ]">get started</a>
+	<section class="[ hero hero-home ]">
+		<div class="[ screen-dark--30 ]">
+			<div class="[ padding-top-bottom--large ]">
+				<div class="[ wrapper ]">
+					<div class="[ text-center color-light ]">
+						<div class="[ bg-image-placeholder__wrapper bg-image-placeholder__wrapper__logo ][ margin-bottom--large ]">
+							<a class="[ bg-image-placeholder bg-image-placeholder__logo ]" href="#"></a>
+						</div>
+						<h1 class="[ text-shadow ]">Bring your imagination to life</h1>
+						<a href="#" class="[ button button--secondary ]">get started</a>
+					</div>
+				</div><!-- wrapper -->
 			</div>
-		</div><!-- wrapper -->
+		</div>
 	</section><!-- hero -->
 
 
@@ -23,8 +27,25 @@
 	<section class="[ video ][ bg-primary ]">
 		<div class="[ wrapper ]">
 			<div class="[ padding-top-bottom--large ][ text-center color-light ]">
-				<video class="[ margin-bottom ]" src="">
-				</video>
+				<div class="[ margin-bottom ][ xmall-12 ][ center ][ relative ]">
+					<video id="video" class="[ xmall-12 ]">
+						<source src="<?php echo THEMEPATH; ?>videos/video1.webm" type="video/webm">
+						<source src="<?php echo THEMEPATH; ?>videos/video1.ogv" type="video/ogv">
+						<source src="<?php echo THEMEPATH; ?>videos/video1.mp4" type="video/mp4">
+						<p>
+							Your browser doesn't support HTML5 video.
+							<a href="<?php echo THEMEPATH; ?>videos/video1.mp4">Download</a> the video instead.
+						</p>
+					</video>
+					<!-- Video Controls -->
+					<div id="video-controls">
+						<button type="button" id="play-pause">Play</button>
+						<input type="range" id="seek-bar" value="0">
+						<button type="button" id="mute">Mute</button>
+						<input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
+						<button type="button" id="full-screen">Full-Screen</button>
+					</div>
+				</div>
 				<h2>UR Imprint Design Studio</h2>
 				<p class="[ italic ]">Direct to garment printing technology for bright long-lasting prints.</p>
 			</div>
@@ -41,19 +62,19 @@
 			<div class="[ row ][ relative ]">
 				<div class="[ column xmall-4 ]">
 					<div class="[ circle border--large bg-secondary ]">
-						<img class="[ svg icon icon--small ][ color-light ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--small ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/shirt.svg" alt="menu">
 					</div>
 					<p class="[ italic ]"><small>Choose</small></p>
 				</div>
 				<div class="[ column xmall-4 ]">
 					<div class="[ circle border--large bg-secondary ]">
-						<img class="[ svg icon icon--small ][ color-light ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--small ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/design.svg" alt="menu">
 					</div>
 					<p class="[ italic ]"><small>Customise</small></p>
 				</div>
 				<div class="[ column xmall-4 ]">
 					<div class="[ circle border--large bg-secondary ]">
-						<img class="[ svg icon icon--small ][ color-light ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--small ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/shop.svg" alt="menu">
 					</div>
 					<p class="[ italic ]"><small>Checkout</small></p>
 				</div>
@@ -71,25 +92,25 @@
 			<div class="[ row ]">
 				<article class="[ square ][ bg-primary--darken-10 ]">
 					<div class="[ square-content ][ color-light ]">
-						<img class="[ svg icon icon--xxlarge ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--xxlarge ]" src="<?php echo THEMEPATH; ?>icons/badge.svg" alt="menu">
 						<p class="[ ]"><small>Premium quality</small></p>
 					</div><!-- square-content -->
 				</article><!-- square -->
 				<article class="[ square ][ bg-primary ]">
 					<div class="[ square-content ][ color-light ]">
-						<img class="[ svg icon icon--xxlarge ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--xxlarge ]" src="<?php echo THEMEPATH; ?>icons/lightning.svg" alt="menu">
 						<p class="[ ]"><small>Fast service</small></p>
 					</div><!-- square-content -->
 				</article><!-- square -->
 				<article class="[ square ][ bg-primary--darken-10 ]">
 					<div class="[ square-content ][ color-light ]">
-						<img class="[ svg icon icon--xxlarge ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--xxlarge ]" src="<?php echo THEMEPATH; ?>icons/shirts.svg" alt="menu">
 						<p class="[ ]"><small>Product variety</small></p>
 					</div><!-- square-content -->
 				</article><!-- square -->
 				<article class="[ square ][ bg-primary ]">
 					<div class="[ square-content ][ color-light ]">
-						<img class="[ svg icon icon--xxlarge ]" src="icons/hamburger.svg" alt="menu">
+						<img class="[ svg icon icon--xxlarge ]" src="<?php echo THEMEPATH; ?>icons/face.svg" alt="menu">
 						<p class="[ ]"><small>Client satisfaction</small></p>
 					</div><!-- square-content -->
 				</article><!-- square -->
