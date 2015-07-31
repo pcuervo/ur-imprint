0b140d9c403b8001301e1df7dbf5d845291b0923
Raúl De Zamacona <raul@zamacona.me>
2015-07-31 17:05:50 -0500

    footer home

--- themes/ur-imprint/footer.php
+++ themes/ur-imprint/footer.php
@@ -41,45 +41,48 @@
 
 		<footer class="">
 			<div class="[ wrapper ]">
-				<div class="[ row ]">
-					<div class="[ xmall-6 large-3 ]">
-						<h3>Payments</h3>
-						<div class="[ row ]">
-							<div class="[ xmall-4 ]">
-								<img class="[ svg icon icon--medium ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/visa.svg" alt="visa">
-							</div>
-							<div class="[ xmall-4 ]">
-								<img class="[ svg icon icon--medium ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/mastercard.svg" alt="mastercard">
-							</div>
-							<div class="[ xmall-4 ]">
-								<img class="[ svg icon icon--medium ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/paypal.svg" alt="paypal">
+				<div class="[ padding-top-bottom ]">
+					<div class="[ row ]">
+						<div class="[ column xmall-6 medium-3 ][ margin-bottom ]">
+							<h3>Payments</h3>
+							<div class="[ row ]">
+								<div class="[ column xmall-4 ]">
+									<img class="[ icon icon--medium ][ image-responsive ]" src="<?php echo THEMEPATH; ?>icons/visa.svg" alt="visa">
+								</div>
+								<div class="[ column xmall-4 ]">
+									<img class="[ icon icon--medium ][ image-responsive ]" src="<?php echo THEMEPATH; ?>icons/mastercard.svg" alt="mastercard">
+								</div>
+								<div class="[ column xmall-4 ]">
+									<img class="[ icon icon--medium ][ image-responsive ]" src="<?php echo THEMEPATH; ?>icons/paypal.svg" alt="paypal">
+								</div>
 							</div>
 						</div>
+						<div class="[ column xmall-6 medium-3 ][ margin-bottom ]">
+							<h3>Contact Us</h3>
+							<p class="[ no-margin ]">Sydney, Australia</p>
+							<p class="[ no-margin ]">(0439) 959 051</p>
+							<p class="[ no-margin ]"><a href="mailto:blabla@gmail.com">Email Us</a></p>
+							<p class="[ no-margin ]"><a href="#">Facebook</a></p>
+						</div>
+						<div class="[ clear--xmall ]"></div>
+						<div class="[ column xmall-6 medium-3 ][ margin-bottom ]">
+							<h3>Quick links</h3>
+							<p class="[ no-margin ]"><a href="#">About Us</a></p>
+							<p class="[ no-margin ]"><a href="#">FAQs</a></p>
+							<p class="[ no-margin ]"><a href="#">Catalogue</a></p>
+							<p class="[ no-margin ]"><a href="#">Get started</a></p>
+						</div>
+						<div class="[ column xmall-6 medium-3 ][ margin-bottom ]">
+							<h3>Designed and developed by</h3>
+							<img class="[ icon icon--medium ]" src="<?php echo THEMEPATH; ?>icons/little-crow.svg" alt="Little Crow">
+						</div>
 					</div>
-					<div class="[ xmall-6 large-3 ]">
-						<h3>Contact Us</h3>
-						<p>Sydney, Australia</p>
-						<p>(0439) 959 051</p>
-						<a href="mailto:blabla@gmail.com">Email Us</a>
-						<a href="#">Facebook</a>
+					<div class="[ text-center ]">
+						<p>© Copyright <?php echo date('Y'); ?> UR Imprint Design Studio and Designs for Business</p>
+						<p class="[ no-margin ]"><a href="#">Privacy Policy</a></p>
+						<p class="[ no-margin ]"><a href="#">Terms and conditions</a></p>
 					</div>
-					<div class="[ xmall-6 large-3 ]">
-						<h3>Quick links</h3>
-						<p><a href="#">About Us</a></p>
-						<p><a href="#">FAQs</a></p>
-						<p><a href="#">Catalogue</a></p>
-						<p><a href="#">Get started</a></p>
-					</div>
-					<div class="[ xmall-6 large-3 ]">
-						<h3>Designed and developed by</h3>
-						<img class="[ svg icon icon--medium ][ color-light ]" src="<?php echo THEMEPATH; ?>icons/little-crow.svg" alt="menu">
-					</div>
-				</div>
-				<div class="[ text-center ]">
-					<p>© Copyright <?php echo date('Y'); ?> UR Imprint Design Studio and Designs for Business</p>
-					<p><a href="#">Privacy Policy</a></p>
-					<p><a href="#">Terms and conditions</a></p>
-				</div>
+				</div><!-- padding-top-bottom -->
 			</div><!-- wrapper -->
 		</footer>
 
