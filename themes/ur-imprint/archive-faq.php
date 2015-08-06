bff0f7f49ecd106b9db7fc3f7e8de4a2d2a16a77
Raúl De Zamacona <raul@zamacona.me>
2015-08-06 14:14:45 -0500

    faqs responsive

--- themes/ur-imprint/archive-faq.php
+++ themes/ur-imprint/archive-faq.php
@@ -9,201 +9,208 @@
 				<h2>Frequently Asked Questions</h2>
 			</div>
 
-			<article class="[ general-faqs ][ margin-bottom--large ]">
-				<?php
-				$faq_counter = 1;
-				$faq_args = array(
-					'post_type' 		=> 'faq',
-					'posts_per_page' 	=> -1,
-					'tax_query'			=> array(
-						array(
-							'taxonomy' 	=> 'faq-categories',
-							'field' 	=> 'slug',
-							'terms'		=> 'general',
-						)
-					),
-				);
-				$faq_query = new WP_Query( $faq_args );
-				if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
-					$faq_cat = get_faq_category( $post->ID );
-					if ( $faq_counter == 1 ){
-						echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
-						echo "<div class='[ accordion ]'>";
-					}
-				?>
-						<div class="[ accordion-section ][ margin-bottom--small ]">
-							<p class="[ lead-text color-primary ]">
-								<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
-									<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
-								</a>
-							</p>
-							<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
-								<p><?php echo get_the_content(); ?></p>
-							</div><!--end .accordion-section-content-->
-						</div><!--end .accordion-section-->
-					<?php
-						$faq_counter++;
-						endwhile; endif; wp_reset_query();
-					?>
-				</div><!-- accordion -->
-			</article>
+			<div class="[ row ]">
 
-			<article class="[ artwork-faqs ][ margin-bottom--large ]">
-				<?php
-				$faq_counter = 1;
-				$faq_args = array(
-					'post_type' 		=> 'faq',
-					'posts_per_page' 	=> -1,
-					'tax_query'			=> array(
-						array(
-							'taxonomy' 	=> 'faq-categories',
-							'field' 	=> 'slug',
-							'terms'		=> 'artwork',
-						)
-					),
-				);
-				$faq_query = new WP_Query( $faq_args );
-				if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
-					$faq_cat = get_faq_category( $post->ID );
-					if ( $faq_counter == 1 ){
-						echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
-						echo "<div class='[ accordion ]'>";
-					}
-				?>
-						<div class="[ accordion-section ][ margin-bottom--small ]">
-							<p class="[ lead-text color-primary ]">
-								<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
-									<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
-								</a>
-							</p>
-							<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
-								<p><?php echo get_the_content(); ?></p>
-							</div><!--end .accordion-section-content-->
-						</div><!--end .accordion-section-->
+				<article class="[ column xmall-12 large-6 ][ general-faqs ][ margin-bottom--large ]">
 					<?php
-						$faq_counter++;
-						endwhile; endif; wp_reset_query();
+					$faq_counter = 1;
+					$faq_args = array(
+						'post_type' 		=> 'faq',
+						'posts_per_page' 	=> -1,
+						'tax_query'			=> array(
+							array(
+								'taxonomy' 	=> 'faq-categories',
+								'field' 	=> 'slug',
+								'terms'		=> 'general',
+							)
+						),
+					);
+					$faq_query = new WP_Query( $faq_args );
+					if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
+						$faq_cat = get_faq_category( $post->ID );
+						if ( $faq_counter == 1 ){
+							echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
+							echo "<div class='[ accordion ]'>";
+						}
 					?>
-				</div><!-- accordion -->
-			</article>
+							<div class="[ accordion-section ][ margin-bottom--small ]">
+								<p class="[ lead-text color-primary ]">
+									<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
+										<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
+									</a>
+								</p>
+								<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
+									<p><?php echo get_the_content(); ?></p>
+								</div><!--end .accordion-section-content-->
+							</div><!--end .accordion-section-->
+						<?php
+							$faq_counter++;
+							endwhile; endif; wp_reset_query();
+						?>
+					</div><!-- accordion -->
+				</article>
 
-			<article class="[ design-tips-faqs ][ margin-bottom--large ]">
-				<?php
-				$faq_counter = 1;
-				$faq_args = array(
-					'post_type' 		=> 'faq',
-					'posts_per_page' 	=> -1,
-					'tax_query'			=> array(
-						array(
-							'taxonomy' 	=> 'faq-categories',
-							'field' 	=> 'slug',
-							'terms'		=> 'design-tips',
-						)
-					),
-				);
-				$faq_query = new WP_Query( $faq_args );
-				if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
-					$faq_cat = get_faq_category( $post->ID );
-					if ( $faq_counter == 1 ){
-						echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
-						echo "<div class='[ accordion ]'>";
-					}
-				?>
-						<div class="[ accordion-section ][ margin-bottom--small ]">
-							<p class="[ lead-text color-primary ]">
-								<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
-									<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
-								</a>
-							</p>
-							<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
-								<p><?php echo get_the_content(); ?></p>
-							</div><!--end .accordion-section-content-->
-						</div><!--end .accordion-section-->
+				<article class="[ column xmall-12 large-6 ][ artwork-faqs ][ margin-bottom--large ]">
 					<?php
-						$faq_counter++;
-						endwhile; endif; wp_reset_query();
+					$faq_counter = 1;
+					$faq_args = array(
+						'post_type' 		=> 'faq',
+						'posts_per_page' 	=> -1,
+						'tax_query'			=> array(
+							array(
+								'taxonomy' 	=> 'faq-categories',
+								'field' 	=> 'slug',
+								'terms'		=> 'artwork',
+							)
+						),
+					);
+					$faq_query = new WP_Query( $faq_args );
+					if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
+						$faq_cat = get_faq_category( $post->ID );
+						if ( $faq_counter == 1 ){
+							echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
+							echo "<div class='[ accordion ]'>";
+						}
 					?>
-				</div><!-- accordion -->
-			</article>
+							<div class="[ accordion-section ][ margin-bottom--small ]">
+								<p class="[ lead-text color-primary ]">
+									<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
+										<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
+									</a>
+								</p>
+								<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
+									<p><?php echo get_the_content(); ?></p>
+								</div><!--end .accordion-section-content-->
+							</div><!--end .accordion-section-->
+						<?php
+							$faq_counter++;
+							endwhile; endif; wp_reset_query();
+						?>
+					</div><!-- accordion -->
+				</article>
 
-			<article class="[ delivery-faqs ][ margin-bottom--large ]">
-				<?php
-				$faq_counter = 1;
-				$faq_args = array(
-					'post_type' 		=> 'faq',
-					'posts_per_page' 	=> -1,
-					'tax_query'			=> array(
-						array(
-							'taxonomy' 	=> 'faq-categories',
-							'field' 	=> 'slug',
-							'terms'		=> 'delivery',
-						)
-					),
-				);
-				$faq_query = new WP_Query( $faq_args );
-				if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
-					$faq_cat = get_faq_category( $post->ID );
-					if ( $faq_counter == 1 ){
-						echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
-						echo "<div class='[ accordion ]'>";
-					}
-				?>
-						<div class="[ accordion-section ][ margin-bottom--small ]">
-							<p class="[ lead-text color-primary ]">
-								<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
-									<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
-								</a>
-							</p>
-							<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
-								<p><?php echo get_the_content(); ?></p>
-							</div><!--end .accordion-section-content-->
-						</div><!--end .accordion-section-->
+				<div class="[ clear--large ]"></div>
+
+				<article class="[ column xmall-12 large-6 ][ design-tips-faqs ][ margin-bottom--large ]">
 					<?php
-						$faq_counter++;
-						endwhile; endif; wp_reset_query();
+					$faq_counter = 1;
+					$faq_args = array(
+						'post_type' 		=> 'faq',
+						'posts_per_page' 	=> -1,
+						'tax_query'			=> array(
+							array(
+								'taxonomy' 	=> 'faq-categories',
+								'field' 	=> 'slug',
+								'terms'		=> 'design-tips',
+							)
+						),
+					);
+					$faq_query = new WP_Query( $faq_args );
+					if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
+						$faq_cat = get_faq_category( $post->ID );
+						if ( $faq_counter == 1 ){
+							echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
+							echo "<div class='[ accordion ]'>";
+						}
 					?>
-				</div><!-- accordion -->
-			</article>
+							<div class="[ accordion-section ][ margin-bottom--small ]">
+								<p class="[ lead-text color-primary ]">
+									<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
+										<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
+									</a>
+								</p>
+								<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
+									<p><?php echo get_the_content(); ?></p>
+								</div><!--end .accordion-section-content-->
+							</div><!--end .accordion-section-->
+						<?php
+							$faq_counter++;
+							endwhile; endif; wp_reset_query();
+						?>
+					</div><!-- accordion -->
+				</article>
 
-			<article class="[ products-garments-sizes-faqs ][ margin-bottom--large ]">
-				<?php
-				$faq_counter = 1;
-				$faq_args = array(
-					'post_type' 		=> 'faq',
-					'posts_per_page' 	=> -1,
-					'tax_query'			=> array(
-						array(
-							'taxonomy' 	=> 'faq-categories',
-							'field' 	=> 'slug',
-							'terms'		=> 'products-garments-sizes',
-						)
-					),
-				);
-				$faq_query = new WP_Query( $faq_args );
-				if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
-					$faq_cat = get_faq_category( $post->ID );
-					if ( $faq_counter == 1 ){
-						echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
-						echo "<div class='[ accordion ]'>";
-					}
-				?>
-						<div class="[ accordion-section ][ margin-bottom--small ]">
-							<p class="[ lead-text color-primary ]">
-								<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
-									<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
-								</a>
-							</p>
-							<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
-								<p><?php echo get_the_content(); ?></p>
-							</div><!--end .accordion-section-content-->
-						</div><!--end .accordion-section-->
+				<article class="[ column xmall-12 large-6 ][ delivery-faqs ][ margin-bottom--large ]">
 					<?php
-						$faq_counter++;
-						endwhile; endif; wp_reset_query();
+					$faq_counter = 1;
+					$faq_args = array(
+						'post_type' 		=> 'faq',
+						'posts_per_page' 	=> -1,
+						'tax_query'			=> array(
+							array(
+								'taxonomy' 	=> 'faq-categories',
+								'field' 	=> 'slug',
+								'terms'		=> 'delivery',
+							)
+						),
+					);
+					$faq_query = new WP_Query( $faq_args );
+					if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
+						$faq_cat = get_faq_category( $post->ID );
+						if ( $faq_counter == 1 ){
+							echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
+							echo "<div class='[ accordion ]'>";
+						}
 					?>
-				</div><!-- accordion -->
-			</article>
+							<div class="[ accordion-section ][ margin-bottom--small ]">
+								<p class="[ lead-text color-primary ]">
+									<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
+										<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
+									</a>
+								</p>
+								<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
+									<p><?php echo get_the_content(); ?></p>
+								</div><!--end .accordion-section-content-->
+							</div><!--end .accordion-section-->
+						<?php
+							$faq_counter++;
+							endwhile; endif; wp_reset_query();
+						?>
+					</div><!-- accordion -->
+				</article>
 
+				<div class="[ clear--large ]"></div>
+
+				<article class="[ column xmall-12 large-6 ][ products-garments-sizes-faqs ][ margin-bottom--large ]">
+					<?php
+					$faq_counter = 1;
+					$faq_args = array(
+						'post_type' 		=> 'faq',
+						'posts_per_page' 	=> -1,
+						'tax_query'			=> array(
+							array(
+								'taxonomy' 	=> 'faq-categories',
+								'field' 	=> 'slug',
+								'terms'		=> 'products-garments-sizes',
+							)
+						),
+					);
+					$faq_query = new WP_Query( $faq_args );
+					if( $faq_query->have_posts() ) : while( $faq_query->have_posts() ) : $faq_query->the_post();
+						$faq_cat = get_faq_category( $post->ID );
+						if ( $faq_counter == 1 ){
+							echo "<h3 class='[ text-center ]'>$faq_cat</h3>";
+							echo "<div class='[ accordion ]'>";
+						}
+					?>
+							<div class="[ accordion-section ][ margin-bottom--small ]">
+								<p class="[ lead-text color-primary ]">
+									<a class="[ color-primary ][ accordion-section-title ]" href="#accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>">
+										<span class="[ toggle ]"></span> <?php echo get_the_title(); ?>
+									</a>
+								</p>
+								<div id="accordion-<?php echo $faq_cat.'-'.$faq_counter; ?>" class="[ accordion-section-content ]">
+									<p><?php echo get_the_content(); ?></p>
+								</div><!--end .accordion-section-content-->
+							</div><!--end .accordion-section-->
+						<?php
+							$faq_counter++;
+							endwhile; endif; wp_reset_query();
+						?>
+					</div><!-- accordion -->
+				</article>
+
+			</div><!-- row -->
 		</div><!-- wrapper -->
 	</section><!-- faqs -->
 
