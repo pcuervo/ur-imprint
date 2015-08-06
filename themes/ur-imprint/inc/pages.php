b60e02ec82f3c838e7ce6b3ac15aef6d7dfa6733
maichomper <maichomper@gmail.com>
2015-08-06 18:51:13 -0500

    add stripe

--- themes/ur-imprint/inc/pages.php
+++ themes/ur-imprint/inc/pages.php
@@ -55,5 +55,17 @@ add_action('init', function(){
 		wp_insert_post( $page, true );
 	}
 
+	// Create
+	if( ! get_page_by_path('checkout') ){
+		$page = array(
+			'post_author' => 1,
+			'post_status' => 'publish',
+			'post_title'  => 'Checkout',
+			'post_name'   => 'checkout',
+			'post_type'   => 'page'
+		);
+		wp_insert_post( $page, true );
+	}
+
 
 });
